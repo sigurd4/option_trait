a8301f897e4b34600ea381b8fb25f77e9c674286
sigurd4 <sigurd.spangelo@gmail.com>
2024-12-22 04:13:42 +0100

    Tried doing something stupid

--- src/not_void.rs
+++ src/not_void.rs
@@ -1,5 +1,13 @@
 use core::{cell::UnsafeCell, marker::PhantomData, mem::ManuallyDrop, ops::{Bound, Range, RangeFrom, RangeInclusive, RangeTo, RangeToInclusive, Yeet}, pin::Pin};
 
+/// A stupid trait made to avoid conflicting implementations.
+/// 
+/// This is supposed to be implemented for anything that isn't `()`, but sometimes it is not, if you make a new struct that contains `()` in any kind of way.
+/// 
+/// If you do, it is highly encouraged to implement this trait for your struct manually, so it can be used with the [`Maybe`](crate::Maybe) trait.
+/// 
+/// Currently this is the best solution i could find to avoid a conflicting implementation, but it may be subject to change later.
+/// Ideally i'd want to get rid of this thing.
 pub auto trait NotVoid {}
 impl !NotVoid for () {}
 
@@ -25,6 +33,36 @@ impl<T> NotVoid for &mut T where T: ?Sized {}
 impl<T> NotVoid for *const T where T: ?Sized {}
 impl<T> NotVoid for *mut T where T: ?Sized {}
 
+// Failed attemts to implement this in a "more" better way.
+
+/*pub trait NotVoid: private::_MaybeVoid<IS_VOID = false>
+{
+
+}
+impl<T> NotVoid for T
+where
+    T: private::_MaybeVoid<IS_VOID = false> + ?Sized
+{
+
+}
+
+mod private
+{
+    pub trait _MaybeVoid
+    {
+        const IS_VOID: bool;
+    }
+    impl<T> _MaybeVoid for T
+    where
+        T: ?Sized
+    {
+        default const IS_VOID: bool = false;
+    }
+    impl _MaybeVoid for ()
+    {
+        const IS_VOID: bool = true;
+    }
+}*/
 
 /*pub trait NotVoid: private::_NotVoid<Void = ()>
 {
